808eca7fe702c9ec65119941d08652a7191d4412
francesco-guzzetta <64249965+francesco-guzzetta@users.noreply.github.com>
2020-05-28 15:47:35 +0200

    Update calcolo_nx.m

--- calcolo_nx.m
+++ calcolo_nx.m
@@ -1,22 +1,16 @@
-function [nx, nx_tot] = calcolo_nx(En, psi, kb, T, autovalori, Ec, Nc)
-
-%% 1D
-
-%abbiamo utilizzato il valore di Ec come valore di riferimento così che
-%al crescere della larghezza della banda il primo autovalore effettivamente
-%converge a zero e quindi il valore di nx viene coerente
+function [nx, nx_tot] = calcolo_nx(En, psi, kb, T, autovalori, Eg);
 
 for i = 1:autovalori
-    Nk(i) = Nc*exp((-(Ec+En(i)))/(kb*T));
-end  
+    f(i) = exp(-(En(i)+(Eg)/2)/(kb*T));
+end
 
 [righe, colonne] = size(psi);   
 nx = zeros(righe,1);
 
 for i = 1:righe
     for j = 1:colonne
-        nx(i) =  nx(i) + Nk(j)*abs(psi(i,j))^2;                                  
+        nx(i) =  nx(i) + f(j)*abs(psi(i,j))^2;                                  
     end                                             
 end  
 
-nx_tot = sum(nx)
+nx_tot = sum(nx);
